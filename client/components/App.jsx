75e7536e2297d42c7b8653bdf9fbd1e59f43a3a6
Billie Staples <billielstaples@gmail.com>
2022-11-04 14:06:18 +1300

    Finally got it thanks Johnny

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,19 +9,10 @@ function App() {
   const [outfits, setOutfits] = useState(data)
 
   return (
-<<<<<<< HEAD
-    <main className="bg-blue-900 w-screen h-screen flex justify-center items-center">
-      <div className="mx-auto ">
-        <header className="mb-20">
-          <h1 className="text-center text-white text-3xl">
-            My Friend&apos;s Outfits
-          </h1>
-=======
     <main className="mainApp">
       <div className="mainOuterDiv">
         <header className="mainHeader">
           <h1 className="mainH1">My Friends Outfits</h1>
->>>>>>> a631643224b4570de64cd9513474755dd3331d48
         </header>
         <div className="mainInnerDiv">
           <Routes>
@@ -31,7 +22,6 @@ function App() {
               element={<Form outfits={outfits} setOutfits={setOutfits} />}
             />
           </Routes>
-          <List />
         </div>
       </div>
     </main>
--- client/components/List.jsx
+++ client/components/List.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 
 function List(props) {
   console.log(props)
+
   return (
     <>
       <Link to="/add">Add New Outfit</Link>
       {props.outfits.map((outfit) => (
-        <div key={outfit.id}>
+        <div key={outfit.id} className="imgDiv">
           <p>{outfit.name}</p>
-          <img src={outfit.top} alt="top"></img>
-          <img src={outfit.bottom} alt="bottom"></img>
+          <img className="clothing" src={outfit.top} alt="top"></img>
+          <img className="clothing" src={outfit.bottom} alt="bottom"></img>
         </div>
       ))}
     </>
